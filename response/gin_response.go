71e92901ebe07b5f9c1a6a96bbc26c379d3a47c5
11SF <nawapong.sit@gmail.com>
2023-08-24 00:37:34 +0700

    add response

---
+++ response/gin_response.go
@@ -0,0 +1,41 @@
+package ginresponse
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type response struct {
+	Code    string      `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type responseError struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func (r *responseError) Error() string {
+	return fmt.Sprintf("status %v: err %v", r.Code, r.Message)
+}
+
+func NewError(code string, err string) error {
+	return &responseError{
+		Code:    code,
+		Message: err,
+	}
+}
+
+func NewResponse(c *gin.Context, httpStatusCode int, data interface{}) {
+	c.JSON(httpStatusCode, response{
+		Code:    "00000",
+		Message: "",
+		Data:    data,
+	})
+}
+
+func NewResponseError(c *gin.Context, httpStatusCode int, err error) {
+	c.JSON(httpStatusCode, err)
+}
